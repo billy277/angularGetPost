a3fded491e277f8dcd0df87689ea75be95760882
billy277 <ashish.adh@gmail.com>
2019-07-27 17:33:52 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GettingComponent } from './getting/getting.component';
+import { PostingComponent } from './posting/posting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GettingComponent,
+    PostingComponent
   ],
   imports: [
     BrowserModule,
