8d1b079b7c0d5874b21b6926c3a3b2d9add4e72d
billy277 <ashish.adh@gmail.com>
2019-08-01 16:31:02 -0500

    testing simplified version of only get request with local server

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -16,32 +16,32 @@ export class HttpService {
      return this.httpClient.request("GET",url,{responseType:"json"});
    }
   
-   _postRequest(url, obj){
-    // console.log("updating the changes variable in the service");
-    // this.changes = true;
-    console.log("inside _post request of the service");
-     return this.httpClient.post( url,obj).subscribe(data => {
-       console.log("POST was sucessful", data);
-      }, error=>{
-        console.log("Error", error);
-      });  
-   }
+  //  _postRequest(url, obj){
+  //   // console.log("updating the changes variable in the service");
+  //   // this.changes = true;
+  //   console.log("inside _post request of the service");
+  //    return this.httpClient.post( url,obj).subscribe(data => {
+  //      console.log("POST was sucessful", data);
+  //     }, error=>{
+  //       console.log("Error", error);
+  //     });  
+  //  }
    
 
-   _putRequest(url, obj){
-     console.log("doing a put request");
-     console.log('url inside put req: ' + url,obj);
-     return this.httpClient.put(url,obj,{responseType:"json"}).subscribe(data=>console.log(data));
-     }
+  //  _putRequest(url, obj){
+  //    console.log("doing a put request");
+  //    console.log('url inside put req: ' + url,obj);
+  //    return this.httpClient.put(url,obj,{responseType:"json"}).subscribe(data=>console.log(data));
+  //    }
    
    
-   _patchRequest(url,index,obj){
-      var urlObj = url + '/'+index 
-      console.log(urlObj);
-    return this.httpClient.patch(url,index,obj).subscribe(data=>{
-      console.log("PATCH request on index: "+ index + " was sucessful with the data: "+ obj);
-    }, error =>{
-      console.log("PATCH error", error);
-    });
-   }
+  //  _patchRequest(url,index,obj){
+  //     var urlObj = url + '/'+index 
+  //     console.log(urlObj);
+  //   return this.httpClient.patch(url,index,obj).subscribe(data=>{
+  //     console.log("PATCH request on index: "+ index + " was sucessful with the data: "+ obj);
+  //   }, error =>{
+  //     console.log("PATCH error", error);
+  //   });
+  //  }
 }
